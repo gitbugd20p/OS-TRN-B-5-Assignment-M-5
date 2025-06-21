97987cb45de66c9e79c688c15ea622abab5623a2
gitbugd20p <155899250+gitbugd20p@users.noreply.github.com>
2025-06-21 18:38:15 +0600

    Fixed live link

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,6 +6,13 @@ const Dashboard = () => {
       <div className="h-screen overflow-hidden rounded-4xl bg-white text-black">
         <div className="p-10">
           <h1 className="text-5xl font-semibold">Dashboard</h1>
+          <h2 className="text-2xl font-medium text-gray-700">
+            Click on{" "}
+            <span className="text-6xl font-bold text-red-600 underline underline-offset-4">
+              Expenses
+            </span>{" "}
+            in the sidebar to view your expense details.
+          </h2>
         </div>
       </div>
     </div>
